916e2d215cf408f4dee7ae4d276e476fc33df068
liulongbin <liulongbin1314@outlook.com>
2024-01-18 14:45:03 +0800

    初始化项目

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import styles from '@/App.module.less'
 
 const App: FC = () => {
   return (
+    // 绑定模块化的 CSS 样式
     <div className={styles.container}>
       <h1 className={styles.title}>App Component</h1>
     </div>
