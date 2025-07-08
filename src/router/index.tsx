fd59bf1eb31c19d2cc1b49d64bb156473b0ccb30
shaoohh <3459143187@qq.com>
2025-07-08 21:42:48 +0800

    feat: complete registration  function

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,13 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 //导入路由组件
-import Reg from "@/views/auth/reg.tsx";
+import Reg, { action as regAction } from "@/views/auth/reg.tsx";
 import Login from "@/views/auth/login.tsx";
 import Root from "@/views/root/root.tsx";
 import AuthLayout from "@/views/auth/auth-layout.tsx";
 const router = createBrowserRouter([
-    {path: '/reg',element: <AuthLayout><Reg /></AuthLayout>},
-    {path: '/login',element: <AuthLayout><Login /></AuthLayout>},
-    {path: '/',element: <Root />},
+    { path: '/reg', action: regAction, element: <AuthLayout><Reg /></AuthLayout> },
+    { path: '/login', element: <AuthLayout><Login /></AuthLayout> },
+    { path: '/', element: <Root /> },
 ])
 
 export default router;
--- src/views/auth/auth-layout.tsx
+++ src/views/auth/auth-layout.tsx
@@ -2,7 +2,7 @@ import { FC , PropsWithChildren} from "react";
 import styles from '@/views/auth/css/auth-layout.module.less';
 const AuthLayout: FC<PropsWithChildren> = ({children}) => {
     return <div className={styles.container}>
-        <div className={styles.box}>{children}</div>
+        <div className={styles.boxTest}>{children}</div>
     </div>
 }
 
